0c029ad38897a4e5a2677714cc89b0ae0f296323
Leticiamatos22 <cristinadematosleticia2@gmail.com>
2023-09-20 22:46:46 -0300

    update

--- tasks/views.py
+++ tasks/views.py
@@ -6,44 +6,15 @@ from .models import Tarefa, Aluno, Disciplina
 from .serializers.tarefaSerializer import TarefaSerializer
 from .serializers.alunoSerializer import AlunoSerializer
 from .serializers.disciplinaSerializer import DisciplinaSerializer
+from rest_framework import generics
 
-class TaskList(APIView):
-    def get(self, request):
-        tasks = Tarefa.objects.all()
-        serializer = TarefaSerializer(tasks, many=True)
-        return Response(serializer.data)
+class TaskList(generics.ListCreateAPIView):
+    queryset = Tarefa.objects.all()
+    serializer_class = TarefaSerializer
 
-    def post(self, request):
-        serializer = TarefaSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class TaskDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Tarefa.objects.get(pk=pk)
-        except Tarefa.DoesNotExist:
-            raise status.HTTP_404_NOT_FOUND
-
-    def get(self, request, pk):
-        task = self.get_object(pk)
-        serializer = TarefaSerializer(task)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        task = self.get_object(pk)
-        serializer = TarefaSerializer(task, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        task = self.get_object(pk)
-        task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tarefa.objects.all()
+    serializer_class = TarefaSerializer
 
 #################### CLASS ALUNO ##########################
 class AlunoList(APIView):
@@ -86,42 +57,20 @@ class AlunoDetail(APIView):
     
 ###################### CLASS DISCIPLINA ######################
 
-class DisciplinaList(APIView):
-    def get(self, request):
-        disciplinas = Disciplina.objects.all()
-        serializer = DisciplinaSerializer(disciplinas, many=True)
-        return Response(serializer.data)
+class DisciplinaList(generics.ListCreateAPIView):
+    queryset = Disciplina.objects.all()
+    serializer_class = DisciplinaSerializer
 
-    def post(self, request):
-        serializer = DisciplinaSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class DisciplinaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Disciplina.objects.all()
+    serializer_class = DisciplinaSerializer
 
-class DisciplinaDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Disciplina.objects.get(pk=pk)
-        except Disciplina.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+###################### CLASS Aluno ######################
 
-    def get(self, request, pk):
-        disciplina = self.get_object(pk)
-        serializer = DisciplinaSerializer(disciplina)
-        return Response(serializer.data)
+class AlunoList(generics.ListCreateAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 
-    def put(self, request, pk):
-        disciplina = self.get_object(pk)
-        serializer = DisciplinaSerializer(disciplina, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        disciplina = self.get_object(pk)
-        disciplina.delete()
-        # Certifique-se de que as tarefas associadas a essa disciplina sejam desassociadas ou excluídas.
-        disciplina.tarefas.clear()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class AlunoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
