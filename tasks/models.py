ffc5a518127d22f4a0a2304a26f7cedb9a8358d3
Leticiamatos22 <cristinadematosleticia2@gmail.com>
2023-09-20 23:01:45 -0300

    feat:  commit comentários

--- tasks/models.py
+++ tasks/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+
 class Aluno(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-
+    
     def __str__(self):
         return self.nome
+     # Classe que representa um aluno.
+
+    # Atributos:
+    # - nome: O nome do aluno (campo de texto com no máximo 100 caracteres).
+    # - email: O endereço de e-mail único do aluno (campo de e-mail).
+
+   
+   
+
 
 class Disciplina(models.Model):
     nome = models.CharField(max_length=100)
@@ -13,6 +23,12 @@ class Disciplina(models.Model):
 
     def __str__(self):
         return self.nome
+    
+    #  Classe que representa uma disciplina.
+
+    # Atributos:
+    # - nome: O nome da disciplina (campo de texto com no máximo 100 caracteres).
+    # - descricao: Uma descrição da disciplina (campo de texto).
 
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=100)
@@ -24,3 +40,13 @@ class Tarefa(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+    # lasse que representa uma tarefa atribuída a um aluno em uma ou mais disciplinas.
+
+    # Atributos:
+    # - titulo: O título da tarefa (campo de texto com no máximo 100 caracteres).
+    # - descricao: Uma descrição mais detalhada da tarefa (campo de texto).
+    # - data_entrega: A data de entrega da tarefa (campo de data).
+    # - concluida: Um campo booleano que indica se a tarefa foi concluída ou não (padrão é False).
+    # - aluno: Uma chave estrangeira que relaciona a tarefa a um aluno específico.
+    # - disciplinas: Uma relação many-to-many que permite associar a tarefa a uma ou mais disciplinas.
--- tasks/views.py
+++ tasks/views.py
@@ -8,6 +8,7 @@ from .serializers.alunoSerializer import AlunoSerializer
 from .serializers.disciplinaSerializer import DisciplinaSerializer
 from rest_framework import generics
 
+# Views relacionadas a Tarefas
 class TaskList(generics.ListCreateAPIView):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
@@ -16,46 +17,18 @@ class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
 
-#################### CLASS ALUNO ##########################
-class AlunoList(APIView):
-    def get(self, request):
-        alunos = Aluno.objects.all()
-        serializer = AlunoSerializer(alunos, many=True)
-        return Response(serializer.data)
+# Views relacionadas a Alunos
 
-    def post(self, request):
-        serializer = AlunoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class AlunoList(generics.ListCreateAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 
-class AlunoDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Aluno.objects.get(pk=pk)
-        except Aluno.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+class AlunoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 
-    def get(self, request, pk):
-        aluno = self.get_object(pk)
-        serializer = AlunoSerializer(aluno)
-        return Response(serializer.data)
 
-    def put(self, request, pk):
-        aluno = self.get_object(pk)
-        serializer = AlunoSerializer(aluno, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        aluno = self.get_object(pk)
-        aluno.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-###################### CLASS DISCIPLINA ######################
+# Views relacionadas a Disciplinas
 
 class DisciplinaList(generics.ListCreateAPIView):
     queryset = Disciplina.objects.all()
@@ -65,12 +38,4 @@ class DisciplinaDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Disciplina.objects.all()
     serializer_class = DisciplinaSerializer
 
-###################### CLASS Aluno ######################
 
-class AlunoList(generics.ListCreateAPIView):
-    queryset = Aluno.objects.all()
-    serializer_class = AlunoSerializer
-
-class AlunoDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Aluno.objects.all()
-    serializer_class = AlunoSerializer
